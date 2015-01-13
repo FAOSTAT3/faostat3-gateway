53697d759888c9fba78faa08ce4a73baadab8ff5
vortex <simone.murzilli@fao.org>
2015-01-13 13:47:22 +0100

    initial commit

---
+++ gateway-repo/faostat/faostat-gateway-js/UIUtils.js
@@ -0,0 +1,24 @@
+if (!window.UIUtils) {
+
+    window.UIUtils = {
+
+        noValuesFoundPanel : function(id) {
+            $("#" + id).empty();
+            $("#" + id).addClass('no-values-panel');
+            $("#" + id).append('No data to display - Please make another selection')
+        },
+
+        initializeDDMenu: function(menuID, dropdownID, dropdownToNotShowID) {
+            $('#' + menuID).on('click touchstart', function () {
+                //$('#' + dropdownID).slideToggle();
+                $('#' + dropdownID).toggle();
+
+                if ( $("#"+ dropdownToNotShowID +":visible") ) {
+                    //$('#' + dropdownToNotShowID).slideUp();
+                    $('#' + dropdownToNotShowID).hide();
+                }
+            });
+        }
+    };
+
+}
